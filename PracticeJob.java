debae39599b95fe41e2571f5f860bb4e11833a8f
Abel Sandoval <abel.sandoval@c02q60kgg8wn-l.corp.lifelock.ad>
2017-07-24 15:46:53 -0700

    recursive method added

--- PracticeJob.java
+++ PracticeJob.java
@@ -2,16 +2,27 @@
 public class PracticeJob {
 
 	public static void main(String[] args) {
-		int x =1;
-		
-		
 		
 		for(int i = 1;i<6;i++)
 		{
 			System.out.println(i+". Hello jenkins job!");
 		}
 		
+		int x = 5;
+		
+		System.out.println("the number for :" + x+" is :"+call(x));
 
 	}
-
+	
+	public static int call(int n)
+	{
+		if(n == 0)
+		{
+			return 0;
+		}
+		else
+		{
+			return n + call(n-1);
+		}
+	}
 }
