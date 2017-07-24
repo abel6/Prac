1aa607e963f2d94bae28964fb6c658ea8e386166
Abel Sandoval <abel.sandoval@c02q60kgg8wn-l.corp.lifelock.ad>
2017-07-24 11:27:14 -0700

    added a loop

--- PracticeJob.java
+++ PracticeJob.java
@@ -2,7 +2,15 @@
 public class PracticeJob {
 
 	public static void main(String[] args) {
-		System.out.println("Hello jenkins job");
+		int x =1;
+		
+		
+		
+		for(int i = 1;i<6;i++)
+		{
+			System.out.println(i+". Hello jenkins job!");
+		}
+		
 
 	}
 
